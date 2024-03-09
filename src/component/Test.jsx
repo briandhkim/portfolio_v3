49fac74ca634e99d016cbb2ad1b911c4147c42cb
Brian Kim <briandhkim93@gmail.com>
2024-03-09 00:54:11 -0800

    Initial setup

---
+++ src/component/Test.jsx
@@ -0,0 +1,12 @@
+const Test = () => {
+    return (
+        <div className='mt-1'>
+            <h1 className='test'>Test</h1>
+            <div>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laudantium quidem, atque incidunt consectetur nemo perspiciatis voluptatem quod rem magnam esse facere odio? Aperiam error cupiditate provident veritatis quos? Tenetur, beatae!            
+            </div>
+        </div>
+    );
+};
+
+export default Test;
