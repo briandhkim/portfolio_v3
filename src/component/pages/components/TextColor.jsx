753a29b7af7d3b94efc6feb467a34605c5a55423
Brian Kim <briandhkim93@gmail.com>
2024-03-10 00:24:51 -0800

    minor adjustment to TextColor comp

--- src/component/pages/components/TextColor.jsx
+++ src/component/pages/components/TextColor.jsx
@@ -7,14 +7,15 @@ const TextColor = () => {
             <div className='mt-5 mb-10 *:mb-4'>
                 <p>
                     The base text color is neutral-400{' '}
-                    <div className='h-4 w-4 bg-neutral-400 inline-block'></div>{' '}
+                    <div className='h-4 w-4 bg-neutral-400 inline-block mx-px'></div>
                     .
                 </p>
                 <p>
                     Here is a text with color{' '}
                     <span className='text-neutral-100'>color emphasis</span>,
-                    which uses neutral-200{' '}
-                    <div className='h-4 w-4 bg-neutral-100 inline-block'></div>{' '}
+                    which uses{' '}
+                    <span className='text-neutral-200'>neutral-200</span> color{' '}
+                    <div className='h-4 w-4 bg-neutral-100 inline-block mx-px'></div>
                     .
                 </p>
             </div>
