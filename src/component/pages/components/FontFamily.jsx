bfb1fb7ac38ad82f2ff2120a294c7d0049a7dc7f
Brian Kim <briandhkim93@gmail.com>
2024-03-10 15:40:38 -0700

    Minor prettier formatting cleanup and font family import

--- src/component/pages/components/FontFamily.jsx
+++ src/component/pages/components/FontFamily.jsx
@@ -2,17 +2,42 @@ import Heading from '../../elements/Heading';
 import DividerTitleLeft from '../../layout/DividerTitleLeft';
 
 const FontFamily = () => {
-    const fonts = ['font-sans', 'font-serif', 'font-mono'];
+    // const fonts = ['font-sans', 'font-serif', 'font-mono'];
+    const fonts = [
+        {
+            category: 'sans',
+            family: 'Ubuntu',
+        },
+        {
+            category: 'serif',
+            family: 'Merriweather',
+        },
+        {
+            category: 'mono',
+            family: 'Roboto Mono',
+        },
+    ];
 
     return (
         <>
             <DividerTitleLeft title='Font family' />
             <div className='mt-5 mb-10 *:mb-4'>
                 {fonts.map(font => (
-                    <Heading key={font} level='h4' className={font}>
-                        <span className='capitalize'>{font}</span>: The quick
-                        brown fox jumps over the lazy dog.
-                    </Heading>
+                    <>
+                        <Heading
+                            key={font.category}
+                            level='h4'
+                            className={`font-${font.category}`}
+                        >
+                            <span className='capitalize underline'>
+                                {font.category}
+                            </span>
+                            : {font.family}
+                        </Heading>
+                        <p className='text-lg sm:text-xl'>
+                            The quick brown fox jumps over the lazy dog.
+                        </p>
+                    </>
                 ))}
             </div>
         </>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
     content: ['./public/index.html', './src/**/*.{html,js,jsx,ts,tsx}'],
     darkMode: 'class',
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ['Ubuntu', ...defaultTheme.fontFamily.sans],
+                serif: ['Merriweather', ...defaultTheme.fontFamily.serif],
+                mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono],
+            },
+        },
     },
     plugins: [require('@tailwindcss/typography')],
 };
