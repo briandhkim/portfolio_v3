5c38d74f8e29ba52a8d01588bb2b29da71183be2
Brian Kim <briandhkim93@gmail.com>
2024-04-07 00:14:52 -0700

    Testing with some side menu items

---
+++ src/component/pages/portfolio/FooterLarge.jsx
@@ -0,0 +1,24 @@
+import ReactIcon from '../../icons/ReactIcon';
+import TailwindIcon from '../../icons/TailwindIcon';
+
+const FooterLarge = () => {
+    return (
+        <div className='flex flex-wrap justify-evenly items-center min-h-32'>
+            <div>
+                Made with &#8192;
+                <div className='h-6 w-6 inline-flex' title='React'>
+                    <span className='sr-only'>React</span>
+                    <ReactIcon />
+                </div>
+                &#8192;&&#8192;
+                <div className='h-6 w-6 inline-flex' title='TailwindCSS'>
+                    <span className='sr-only'>TailwindCSS</span>
+                    <TailwindIcon />
+                </div>
+            </div>
+            <div>&copy; {new Date().getFullYear()} | Dong Hyun Kim</div>
+        </div>
+    );
+};
+
+export default FooterLarge;
--- src/component/pages/portfolio/Portfolio.jsx
+++ src/component/pages/portfolio/Portfolio.jsx
@@ -1,3 +1,4 @@
+import ColLeft from './ColLeft';
 import Navbar from './Navbar';
 
 const Portfolio = () => {
@@ -6,12 +7,11 @@ const Portfolio = () => {
             <Navbar />
             <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 '>
                 {/* We've used 3xl here, but feel free to try other max-widths based on your needs */}
+                {/* May need to remove max-h-svh here */}
                 <div className='mx-auto px-4 sm:px-6 lg:px-8 min-h-svh max-h-svh overflow-y-auto '>
                     <div className='grid grid-cols-3 auto-rows-min md:auto-rows-auto min-h-svh pt-16 '>
-                        <div className='col-span-3 md:col-span-1 md:border-r-2 border-neutral-200 '>
-                            <h1>col left</h1>
-                        </div>
-                        <div className='col-span-3 md:col-span-2 md:border-l-2 border-neutral-200 '>
+                        <ColLeft />
+                        <div className='col-span-3 md:col-span-2 md:border-l-2 md:border-r-4 border-neutral-200 '>
                             <h1>col right</h1>
                         </div>
                     </div>
