232906a19c314531cc5010646e3376001531c06d
Brian Kim <briandhkim93@gmail.com>
2024-09-25 23:01:08 -0700

    Update ImageExampleModal to make it scrollable

--- src/component/pages/bh-presentation/sections/partials/ImageExampleModal.jsx
+++ src/component/pages/bh-presentation/sections/partials/ImageExampleModal.jsx
@@ -17,30 +17,32 @@ const ImageExampleModal = ({ isOpen, setIsOpen, feature }) => {
 			className='relative z-50 focus:outline-none'
 		>
 			<DialogBackdrop className='fixed inset-0 bg-[#325343]/85' />
-			<div className='fixed inset-0 flex w-screen items-center justify-center p-4'>
-				<DialogPanel className='max-w-5xl space-y-4 rounded-xl border border-neutral-800 bg-[#f7f0eb] text-neutral-700 p-12'>
-					<DialogTitle className='font-bold text-2xl font-sans'>
-						{name}
-					</DialogTitle>
-					<Description></Description>
-					<div className='flex justify-center'>
-						<img
-							src={imageSrc}
-							alt={imageAlt}
-							className='size-11/12  object-center shadow-2xl'
-						/>
-					</div>
+			<div className='fixed inset-0 w-screen overflow-y-auto p-4'>
+				<div className='flex min-h-full items-center justify-center p-4'>
+					<DialogPanel className='max-w-5xl space-y-4 rounded-xl border border-neutral-800 bg-[#f7f0eb] text-neutral-700 p-12'>
+						<DialogTitle className='font-bold text-2xl font-sans'>
+							{name}
+						</DialogTitle>
+						<Description></Description>
+						<div className='flex justify-center'>
+							<img
+								src={imageSrc}
+								alt={imageAlt}
+								className='size-11/12  object-center shadow-2xl'
+							/>
+						</div>
 
-					<div className='flex gap-4 pt-8 px-8 justify-end'>
-						<SolidButton
-							clickHandler={() => setIsOpen(false)}
-							size='sm'
-							className='rounded'
-						>
-							Close
-						</SolidButton>
-					</div>
-				</DialogPanel>
+						<div className='flex gap-4 pt-8 px-8 justify-end'>
+							<SolidButton
+								clickHandler={() => setIsOpen(false)}
+								size='sm'
+								className='rounded'
+							>
+								Close
+							</SolidButton>
+						</div>
+					</DialogPanel>
+				</div>
 			</div>
 		</Dialog>
 	);
